065e9903aa38ac05848f1aac57aa1c5f14734b1a
Gunar C. Gessner <gunar@gunargessner.com>
2017-05-05 12:09:59 -0300

    feat: fix links to posts as well

--- index.js
+++ index.js
@@ -21,19 +21,25 @@
   function goToMessage(ev) {
     const anchor = ev.target
     const url = $(anchor).data('referer-original-href')
-    if (!url || !url.match(/\/\/[^.]+.slack.com\/(archives|messages)/g) || url.includes(window.location.origin)) return
+    if (!url) return
+    if (url.includes(window.location.origin)) return
 
-    ev.preventDefault();
-    const id = TS.utility.getChannelNameFromUrl(url)
-    const ts = Number(TS.utility.getPathFromSlackUrl(url)[2].substr(1))/1000000
-    const url_vars = getQueryParams(url)
+    if (url.match(/\/\/[^.]+.slack.com\/(archives|messages)/g)) {
+      ev.preventDefault();
+      const id = TS.utility.getChannelNameFromUrl(url)
+      const ts = Number(TS.utility.getPathFromSlackUrl(url)[2].substr(1))/1000000
+      const url_vars = getQueryParams(url)
 
-    // No thread_ts given, so we're linking to a message
-    if (!url_vars.thread_ts) return TS.client.ui.tryToJump(id, ts)
+      // No thread_ts given, so we're linking to a message
+      if (!url_vars.thread_ts) return TS.client.ui.tryToJump(id, ts)
 
-    // We are linking to a thread reply
-    const model_ob = TS.shared.getModelObById(url_vars.cid)
-    TS.ui.replies.openConversation(model_ob, url_vars.thread_ts)
+      // We are linking to a thread reply
+      const model_ob = TS.shared.getModelObById(url_vars.cid)
+      TS.ui.replies.openConversation(model_ob, url_vars.thread_ts)
+    }
+    else if (url.match(/\/\/[^.]+.slack.com\/files/g)) {
+      anchor.href = url.replace(/^.*\.slack.com/, window.location.origin)
+    }
   }
 
   // https://www.w3.org/TR/selectors/#attribute-substrings
